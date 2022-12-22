7b87cfd2f0e66df5793ddf8b72bd332dacd3c0a2
Piotr Krzysztof <piotrlis555@gmail.com>
2022-12-22 16:53:12 +0100

    feat: completely moved the options

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -2,8 +2,11 @@
 --   vim.api.nvim_set_option_value('vimtex_compiler_latexmk.options', '-shell-escape')
 --   vim.g.vimtex_view_general_viewer = 'zathura'
 -- end
+--
 
 local load_core = function ()
+  require'impatient'.enable_profile()
+
   require'core.packer'
   require'core.options'
   require'core.mappings'
--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -1,15 +1,19 @@
+local config = require'config'
+
 return require'packer'.startup(function (use)
-  -- plugin manager
   use {'wbthomason/packer.nvim'}
 
   use {'lewis6991/impatient.nvim',
-    config = [[require('config.impatient')]]
+    config = config.impatient
   }
 
-  -- treesitter
   use { 'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate',
-    config = [[require('config.treesitter')]]
+    config = config.impatient
+  }
+
+  use { 'neovim/nvim-lspconfig',
+    config = config.lsp
   }
 
 end)
