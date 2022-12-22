85a3d6e7b915616221fa81156632e5c1ee9c1741
Piotr Krzysztof <piotrlis555@gmail.com>
2022-12-22 16:33:24 +0100

    feat: added impatient

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -2,6 +2,10 @@ return require'packer'.startup(function (use)
   -- plugin manager
   use {'wbthomason/packer.nvim'}
 
+  use {'lewis6991/impatient.nvim',
+    config = [[require('config.impatient')]]
+  }
+
   -- treesitter
   use { 'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate',
