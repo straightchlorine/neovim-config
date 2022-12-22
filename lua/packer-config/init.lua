76f004b72169aa8404beb15929ae67d5a2fe36b3
Piotr Krzysztof <piotrlis555@gmail.com>
2022-12-22 12:03:13 +0100

    removed most of the plugins

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,4 +1,8 @@
 return require('packer').startup(function(use)
+  -- plugin managers
+  use {'wbthomason/packer.nvim'}
+  use {'neoclide/coc.nvim', branch = 'release', config=[[require('config.coc')]]}
+
   -- module loader
   use { 'lewis6991/impatient.nvim',
     config = [[require('config.impatient')]]
@@ -10,23 +14,9 @@ return require('packer').startup(function(use)
    config = [[require('config.treesitter')]]
   }
 
-  -- plugin managers
-  use {'wbthomason/packer.nvim'}
-  use {'neoclide/coc.nvim', branch = 'release', config=[[require('config.coc')]]}
 
   use { 'neovim/nvim-lspconfig', config = [[require('config.lsp')]] }
   use { 'j-hui/fidget.nvim', config = [[require('config.fidget-nvim')]] }
-  use({ 'glepnir/lspsaga.nvim',
-    branch = "main",
-    config = function()
-        local saga = require("lspsaga")
-
-        saga.init_lsp_saga({
-            -- your configuration
-        })
-    end,
-  })
-
 
   use {'SirVer/ultisnips',
       requires = {{'honza/vim-snippets', rtp = '.'}},
@@ -62,15 +52,6 @@ return require('packer').startup(function(use)
     config = [[require('config.nvim-cmp')]] })
 
 
-  -- snippets
-  -- autocompletion & sources
-  use { 'kevinhwang91/nvim-bqf', ft = 'qf', config = [[require('config.bqf')]] }
-
-  use { 'nvim-telescope/telescope.nvim',
-    cmd = 'Telescope',
-    requires = { { 'nvim-lua/plenary.nvim' } },
-  }
-
   use {'junegunn/fzf', run = function()
       vim.fn['fzf#install']()
   end}
@@ -80,11 +61,6 @@ return require('packer').startup(function(use)
   -- lsp
   use { 'mfussenegger/nvim-jdtls' }
   use { 'mfussenegger/nvim-dap' }
-  use { 'lervag/vimtex' }
-  use { 'ii14/emmylua-nvim', ft = 'lua' }
-
-  use { 'gelguy/wilder.nvim', opt = true, setup = [[vim.cmd('packadd wilder.nvim')]] }
-
   use { 'nvim-lualine/lualine.nvim',
     requires = {
       'kyazdani42/nvim-web-devicons',
@@ -93,21 +69,6 @@ return require('packer').startup(function(use)
       event = 'VimEnter'
     }
   }
-
-  use { 'akinsho/bufferline.nvim', event = 'VimEnter',
-      config = [[require('config.bufferline')]]
-  }
-
-  -- themes
-  use "EdenEast/nightfox.nvim"
-  vim.cmd('colorscheme carbonfox')
-
-  use {
-    'lukas-reineke/indent-blankline.nvim',
-    event = 'VimEnter',
-    config = [[require('config.indent-blankline')]],
-  }
-
   use {
     'rcarriga/nvim-notify',
     event = 'BufEnter',
@@ -118,79 +79,7 @@ return require('packer').startup(function(use)
     end,
   }
 
-  use { 'Raimondi/delimitMate', event = 'InsertEnter' }
-
-  use { 'tpope/vim-commentary', event = 'VimEnter' }
-  use { 'tpope/vim-fugitive', event = 'User InGitRepo', config = [[require('config.fugitive')]] }
-  use { 'rbong/vim-flog', requires = 'tpope/vim-fugitive', cmd = { 'Flog' } }
-  use { 'christoomey/vim-conflicted', requires = 'tpope/vim-fugitive', cmd = { 'Conflicted' } }
-
-  use {
-    'ruifm/gitlinker.nvim',
-    requires = 'nvim-lua/plenary.nvim',
-    event = 'User InGitRepo',
-    config = [[require('config.git-linker')]],
-  }
-
-  use { 'simnalamburt/vim-mundo', cmd = { 'MundoToggle', 'MundoShow' } }
-
-  use { 'tpope/vim-eunuch', cmd = { 'Rename', 'Delete' } }
-
-
-  use { 'lewis6991/gitsigns.nvim', config = [[require('config.gitsigns')]] }
-
-  use { 'rhysd/committia.vim', opt = true, setup = [[vim.cmd('packadd committia.vim')]] }
-
-
-  use { 'preservim/vim-markdown', ft = { 'markdown' } }
-  use { 'vim-pandoc/vim-markdownfootnotes', ft = { 'markdown' } }
-
-  use { 'godlygeek/tabular', cmd = { 'Tabularize' } }
-
-  use { 'elzr/vim-json', ft = { 'json', 'markdown' } }
-
-  use { 'folke/zen-mode.nvim', cmd = 'ZenMode', config = [[require('config.zen-mode')]] }
-
-  use { 'chrisbra/unicode.vim', event = 'VimEnter' }
-
-  use { 'wellle/targets.vim', event = 'VimEnter' }
-
-  use { 'machakann/vim-sandwich', event = 'VimEnter' }
-
-  use { 'michaeljsmith/vim-indent-object', event = 'VimEnter' }
-
-  use { 'tmux-plugins/vim-tmux', ft = { 'tmux' } }
-
-  use { 'andymass/vim-matchup', event = 'VimEnter' }
-
   use { 'mhartington/formatter.nvim' }
-
-  use {
-    'karb94/neoscroll.nvim',
-    event = 'VimEnter',
-    config = function()
-      require('neoscroll').setup({
-          -- All these keys will be mapped to their corresponding default scrolling animation
-          mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
-                      '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
-          hide_cursor = true,          -- Hide cursor while scrolling
-          stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-          respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-          cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-          easing_function = nil,       -- Default easing function
-          pre_hook = nil,              -- Function to run before the scrolling animation starts
-          post_hook = nil,             -- Function to run after the scrolling animation ends
-          performance_mode = false,    -- Disable "Performance Mode" on all buffers.
-      })
-    end,
-  }
-  use { 'skywind3000/asyncrun.vim', opt = true, cmd = { 'AsyncRun' } }
-
-  use { 'tpope/vim-obsession', cmd = 'Obsession' }
-
-  use { 'ojroques/vim-oscyank', cmd = { 'OSCYank', 'OSCYankReg' } }
-
-  use { 'jdhao/whitespace.nvim', event = 'VimEnter' }
 
   use {
     'kyazdani42/nvim-tree.lua',
