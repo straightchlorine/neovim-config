c3ec66ae55e1031ec4dd6426a2567a684c4c4fef
Piotr Krzysztof <piotrlis555@gmail.com>
2022-12-24 14:36:23 +0100

    added the mappings for fugitive, telescope and fzf (initial)

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,10 +1,35 @@
+local map = vim.api.nvim_set_keymap
+
 -- leader key
-vim.g.mapleader = "<space>"
+map('n', '<Space>', '', {})
+vim.g.mapleader = ' '
+
+local options = { noremap = true }
 
 -- switching windows
-vim.keymap.set("n", "<left>", "<c-w>h")
-vim.keymap.set("n", "<Right>", "<C-W>l")
-vim.keymap.set("n", "<Up>", "<C-W>k")
-vim.keymap.set("n", "<Down>", "<C-W>j")
+map('n', '<left>', '<c-w>h', options)
+map('n', '<right>', '<c-w>l', options)
+map('n', '<up>', '<c-w>k', options)
+map('n', '<down>', '<c-w>j', options)
 
-vim.keymap.set('n', '<leader>sv', '<cmd>source $MYVIMRC<cr>')
+-- splitting the buffer
+map('n', '<leader>vs', ':vsplit', options)
+map('n', '<leader>hs', ':split', options)
+
+map( 'n', '<leader><esc>', ':nohlsearch<cr>', options)
+map( 'n', '<C-n>', ':NvimTreeToggle<cr>', options)
+map( 'n', '<leader>gg', ':Git<cr>', options)
+
+-- Fugitive bindings
+map( 'n', '<leader>ga', ':Gwrite<cr>', options)
+map( 'n', '<leader>gp', ':Git push<cr>', options)
+map( 'n', '<leader>gc', ':Git commit<cr>', options)
+
+-- Telescope bindings
+map( 'n', '<leader>p', ':Telescope project<cr>', options)
+map( 'n', '<leader>b', ':Telescope buffers<cr>', options)
+map( 'n', '<leader>f', ':Telescope find_files<cr>', options)
+
+-- fzf bindings
+map( 'n', '<leader>gf', ':GitFZF<cr>', options)
+map( 'n', '<leader>ff', ':DirFZF<cr>', options)
