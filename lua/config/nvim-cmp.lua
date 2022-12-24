09dfe7b35d0259cfb4f9fb99d5fd43bce027a9d3
Piotr Krzysztof <piotrlis555@gmail.com>
2022-12-24 23:42:26 +0100

    feat: changed the highlighting of the completion window

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -7,35 +7,62 @@ local lspkind = require'lspkind'
 
 cmp.setup({
   formatting = {
-    format = function(entry, vim_item)
-      vim_item.mode = ({'symbol_text'})
-      vim_item.menu = ({
-          buffer = "β",
-          bufferlines = "Β",
-          calc = "κ",
-          dictionary = "Δ",
-          digraphs = "Γ",
-          emoji = "ω",
-          nvim_lsp = "λ",
-          nvim_lsp_signature_help = "Λ",
-          nvim_lua = "ι",
-          omni = "Ω",
-          path = "ζ",
-          ultisnips = "Θ",
-          spell = "φ",
-          cmdline = "Σ"
-      })[entry.source.name]
+    format = lspkind.cmp_format({
+      mode = 'text_symbol',
+      menu = ({
+        buffer = "β",
+        bufferlines = "Β",
+        calc = "κ",
+        dictionary = "Δ",
+        digraphs = "Γ",
+        emoji = "ω",
+        nvim_lsp = "λ",
+        nvim_lsp_signature_help = "Λ",
+        nvim_lua = "ι",
+        omni = "Ω",
+        path = "ζ",
+        ultisnips = "Θ",
+        spell = "φ",
+        cmdline = "Σ"
+        })
+      }
+    ),
 
-      if vim.tbl_contains({ 'path' }, entry.source.name) then
-        local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
-        if icon then
-          vim_item.kind = icon
-          vim_item.kind_hl_group = hl_group
-          return vim_item
-        end
-      end
-      return lspkind.cmp_format({ with_text = false })(entry, vim_item)
-    end
+    --format = function(entry, vim_item)
+    --  vim_item.mode = ({'symbol_text'})
+    --  vim_item.menu = ({
+    --      buffer = "β",
+    --      bufferlines = "Β",
+    --      calc = "κ",
+    --      dictionary = "Δ",
+    --      digraphs = "Γ",
+    --      emoji = "ω",
+    --      nvim_lsp = "λ",
+    --      nvim_lsp_signature_help = "Λ",
+    --      nvim_lua = "ι",
+    --      omni = "Ω",
+    --      path = "ζ",
+    --      ultisnips = "Θ",
+    --      spell = "φ",
+    --      cmdline = "Σ"
+    --  })[entry.source.name]
+
+    --  if vim.tbl_contains({ 'path' }, entry.source.name) then
+    --    local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
+    --    if icon then
+    --      vim_item.kind = icon
+    --      vim_item.kind_hl_group = hl_group
+    --      return vim_item
+    --    end
+    --  end
+    --  return lspkind.cmp_format({ with_text = false })(entry, vim_item)
+    --end
+  },
+  view = {
+    entries = {
+      name = 'custom',
+      selection_order = 'near_cursor',
+    }
   },
   enabled = function()
     -- disable completion in comments
@@ -54,14 +81,12 @@ cmp.setup({
     end,
   },
   window = {
-
-
-
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-    winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-    col_offset = -3,
-    side_padding = 0,
+    completion = {
+      border = 'none',
+    },
+    documentation = {
+      border = 'none',
+    },
   },
   sources = cmp.config.sources({
     { 
@@ -79,7 +104,6 @@ cmp.setup({
     },
     { name = 'calc' },
     { name = 'dictionary' },
-    { name = 'digraphs' },
     { name = 'emoji', insert = true },
     { name = 'nvim_lsp_signature_help' },
     { name = 'nvim_lua' },
@@ -214,7 +238,7 @@ cmp.setup({
   })
 
 cmp.setup.cmdline({ "/", "?" }, {
-    --mapping = cmp.mapping.preset.cmdline(),
+    mapping = cmp.mapping.preset.cmdline(),
     sources = {
         {
             name = "buffer",
@@ -234,21 +258,35 @@ cmp.setup.cmdline(':', {
     })
 })
 
-vim.cmd([[
-  " gray
-  highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
-  " blue
-  highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
-  highlight! link CmpItemAbbrMatchFuzzy CmpItemAbbrMatch
-  " light blue
-  highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
-  highlight! link CmpItemKindInterface CmpItemKindVariable
-  highlight! link CmpItemKindText CmpItemKindVariable
-  " pink
-  highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
-  highlight! link CmpItemKindMethod CmpItemKindFunction
-  " front
-  highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
-  highlight! link CmpItemKindProperty CmpItemKindKeyword
-  highlight! link CmpItemKindUnit CmpItemKindKeyword
-]])
+-- highlighting
+vim.api.nvim_set_hl(0, 'PmenuSel',                  { bg = "#282C34", fg = "NONE" })
+vim.api.nvim_set_hl(0, 'Pmenu',                     { fg = "#C5CDD9", bg = "#22252A" })
+vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated',     { fg = "#7E8294", bg = "NONE", strikethrough = true })
+vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch',          { fg = "#82AAFF", bg = "NONE", bold = true,  } )
+vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy',     { fg = "#82AAFF", bg = "NONE", bold = true,  })
+vim.api.nvim_set_hl(0, 'CmpItemMenu',               { fg = "#C792EA", bg = "NONE", italic = true })
+vim.api.nvim_set_hl(0, 'CmpItemKindField',          { fg = "#EED8DA", bg = "#B5585F" })
+vim.api.nvim_set_hl(0, 'CmpItemKindProperty',       { fg = "#EED8DA", bg = "#B5585F" })
+vim.api.nvim_set_hl(0, '',                          { fg = "#EED8DA", bg = "#B5585F" })
+vim.api.nvim_set_hl(0, 'CmpItemKindText',           { fg = "#C3E88D", bg = "#9FBD73" })
+vim.api.nvim_set_hl(0, 'CmpItemKindEnum',           { fg = "#C3E88D", bg = "#9FBD73" })
+vim.api.nvim_set_hl(0, 'CmpItemKindKeyword',        { fg = "#C3E88D", bg = "#9FBD73" })
+vim.api.nvim_set_hl(0, 'CmpItemKindConstant',       { fg = "#FFE082", bg = "#D4BB6C" })
+vim.api.nvim_set_hl(0, 'CmpItemKindConstructor',    { fg = "#FFE082", bg = "#D4BB6C" })
+vim.api.nvim_set_hl(0, 'CmpItemKindReference',      { fg = "#FFE082", bg = "#D4BB6C" })
+vim.api.nvim_set_hl(0, 'CmpItemKindFunction',       { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, 'CmpItemKindStruct',         { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, 'CmpItemKindClass',          { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, 'CmpItemKindModule',         { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, 'CmpItemKindOperator',       { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, 'CmpItemKindVariable',       { fg = "#C5CDD9", bg = "#7E8294" })
+vim.api.nvim_set_hl(0, 'CmpItemKindFile',           { fg = "#C5CDD9", bg = "#7E8294" })
+vim.api.nvim_set_hl(0, 'CmpItemKindUnit',           { fg = "#F5EBD9", bg = "#D4A959" })
+vim.api.nvim_set_hl(0, 'CmpItemKindSnippet',        { fg = "#F5EBD9", bg = "#D4A959" })
+vim.api.nvim_set_hl(0, 'CmpItemKindFolder',         { fg = "#F5EBD9", bg = "#D4A959" })
+vim.api.nvim_set_hl(0, 'CmpItemKindMethod',         { fg = "#DDE5F5", bg = "#6C8ED4" })
+vim.api.nvim_set_hl(0, 'CmpItemKindValue',          { fg = "#DDE5F5", bg = "#6C8ED4" })
+vim.api.nvim_set_hl(0, 'CmpItemKindEnumMember',     { fg = "#DDE5F5", bg = "#6C8ED4" })
+vim.api.nvim_set_hl(0, 'CmpItemKindInterface',      { fg = "#D8EEEB", bg = "#58B5A8" })
+vim.api.nvim_set_hl(0, 'CmpItemKindColor',          { fg = "#D8EEEB", bg = "#58B5A8" })
+vim.api.nvim_set_hl(0, 'CmpItemKindTypeParameter',{ fg = "#D8EEEB", bg = "#58B5A8" })
